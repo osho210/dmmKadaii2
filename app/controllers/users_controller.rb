a35d9bd90149daee9ba121bcbbbd65b4d9e2fdf3
osho210 <anysense.yamatsu@gmail.com>
2022-11-05 05:52:41 +0000

    [Add] dbリセット前

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+
+    # 正常に処理されるとフラッシュメッセージ
+        @user = User.new(user_params)
+	      @user.save!
+	      redirect_to user_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users ,only: [:show,:edit , :create ,:edit , :destroy];
+  resources :users ,only: [:show,:edit , :create , :destroy, :update];
   resources :books ,only: [:new , :index , :show ,:create ,:edit , :destroy];
   get 'homes/about' ,  to: 'homes#about' , as: 'about'
   root to: 'homes#top'
